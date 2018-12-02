0120cd440f491dcea6927a0110913acb312ecaed
arib7701 <amandineribot01@gmail.com>
2018-12-02 11:41:44 +0100

    add view controller

---
+++ src/main/java/com/bankapp/controller/AngularController.java
@@ -0,0 +1,13 @@
+package com.bankapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AngularController {
+
+    @RequestMapping({"/admin", "/account/**", "/profile/**", "/transfer/**"})
+    public String index() {
+        return "forward:/index.html";
+    }
+}
