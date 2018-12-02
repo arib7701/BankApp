46c7a527cb63c44d04ee72277718b241b83159b9
arib7701 <amandineribot01@gmail.com>
2018-12-02 11:47:50 +0100

    add view routes for deployment

--- src/main/java/com/bankapp/controller/AngularController.java
+++ src/main/java/com/bankapp/controller/AngularController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AngularController {
 
-    @RequestMapping({"/admin", "/account/**", "/profile/**", "/transfer/**"})
+    @RequestMapping({"/admin/**", "/account/**", "/profile/**", "/transfer/**", "/login"})
     public String index() {
         return "forward:/index.html";
     }
